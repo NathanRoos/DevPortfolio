1ca6d15fc099aa67022a66dff72151404b2e9914
Jeefos <nathan.w.roos@gmail.com>
2026-02-22 07:34:12 -0500

    fix

--- src/app/admin/testimonials/page.tsx
+++ src/app/admin/testimonials/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState, useEffect } from 'react';
+import { useLanguage } from '../../../context/LanguageContext';
 import AdminGuard from '../../../components/AdminGuard';
 
 interface Testimonial {
